d938edc4fd1a43f89ee3a24ceb76c547be29d3e1
Artaz <callmeartaz@gmail.com>
2023-07-08 10:13:35 +0330

    all components coded and items dynamically adding to the dom

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent {
-
+  selected: string = 'allCategory';
+  constructor(private renderer: Renderer2) {}
+  ngOnInit(): void {
+    const firstItemBackground =
+      document.getElementsByClassName('allCategoryLi')[0];
+    const firstItemText = document.getElementsByClassName('allCategoryTxt')[0];
+    this.renderer.setStyle(firstItemBackground, 'background', '#F1F6F9');
+    this.renderer.setStyle(firstItemText, 'color', '#393053');
+  }
+  setSeleted(selected: string): void {
+    if (selected === this.selected) {
+      return;
+    }
+    const oldSelectedBackground: Element = document.getElementsByClassName(
+      `${this.selected}Li`
+    )[0];
+    const oldSelectedText: Element = document.getElementsByClassName(
+      `${this.selected}Txt`
+    )[0];
+    const newSelectedBackground: Element = document.getElementsByClassName(
+      `${selected}Li`
+    )[0];
+    const newSelectedText: Element = document.getElementsByClassName(
+      `${selected}Txt`
+    )[0];
+    this.renderer.setStyle(oldSelectedBackground, 'background', '#393053');
+    this.renderer.setStyle(oldSelectedText, 'color', '#fff');
+    this.renderer.setStyle(newSelectedBackground, 'background', '#F1F6F9');
+    this.renderer.setStyle(newSelectedText, 'color', '#393053');
+    this.selected = selected;
+    return;
+  }
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ViewContainerRef,
+  AfterContentInit,
+  OnInit,
+} from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/product';
+import { ProductItemComponent } from '../product-item/product-item.component';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   products: Product[] = [];
+  @ViewChild('container', { read: ViewContainerRef })
+  container!: ViewContainerRef;
   constructor(private productService: ProductService) {}
   ngOnInit(): void {
-    this.productService.getProducts.subscribe((products) => {
+    this.productService.getProducts.subscribe((products: Product[]) => {
       this.products = products;
+      this.container.clear();
+      for (let p of this.products) {
+        let pi = this.container.createComponent(ProductItemComponent);
+        pi.instance.product = p;
+      }
+      // console.log(this.products);
     });
-    console.log(this.products);
   }
 }
